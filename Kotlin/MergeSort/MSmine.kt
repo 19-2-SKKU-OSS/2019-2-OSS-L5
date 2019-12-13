b4e81ba8cf9c38f8c33f5b726d00d807596a39cb
mikeysw <50179740+mikeysw@users.noreply.github.com>
2019-12-13 23:52:53 +0900

    Update MSmine.kt

--- Kotlin/MergeSort/MSmine.kt
+++ Kotlin/MergeSort/MSmine.kt
@@ -1,9 +1,8 @@
 package sort
 
 fun main(args: Array<String>){
-    val array = mutableListOf(24, 3, 5, 19, 200, 51) // original array
-    
-    val sorted_array = merge_sort(array) // array after sorting
+    val array = mutableListOf(24, 3, 5, 19, 200, 51) // original array 원래 배열
+    val sorted_array = merge_sort(array) // array after sorting 정렬 후 배열
     print("sorted array = $sorted_array")
 
 }
@@ -16,7 +15,7 @@ fun merge_sort(array: MutableList<Int>): MutableList<Int> {
     val left = merge_sort(array.subList(0,mid))
     val right = merge_sort(array.subList(mid, len))
 
-    return merge(left, right) // merge left half and right half
+    return merge(left, right) // merge left half and right half 왼쪽 반과 오른쪽 반 합치기
 
 }
 
@@ -25,7 +24,7 @@ fun merge(left: MutableList<Int>, right: MutableList<Int>): MutableList<Int>{
     var lfidx=0
     var riidx=0
 
-    while(lfidx<left.size && riidx<right.size){ // compare one by one of left and right
+    while(lfidx<left.size && riidx<right.size){ // compare one by one of left and right 왼쪽 거랑 오른쪽 거 하나씩 비교
         val lh=left[lfidx]
         val rh=right[riidx]
         if(lh<rh){
@@ -36,7 +35,7 @@ fun merge(left: MutableList<Int>, right: MutableList<Int>): MutableList<Int>{
             riidx++}
     }
 
-    // after finishing one side
+    // after finishing one side 한쪽 다 사용하면
     while(lfidx<left.size){
         result.add(left[lfidx])
         lfidx++
