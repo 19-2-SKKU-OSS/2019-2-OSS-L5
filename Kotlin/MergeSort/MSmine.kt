568ac344a6c712d8b6147bfbfd77c00d9181d9b6
Sangwon skkusosc <mikeyoun2000@gmail.com>
2019-12-13 22:13:17 +0900

    write comments in English

--- Kotlin/MergeSort/MSmine.kt
+++ Kotlin/MergeSort/MSmine.kt
@@ -1,22 +1,22 @@
 package sort
 
 fun main(args: Array<String>){
-    val array = mutableListOf(24, 3, 5, 19, 200, 51)
+    val array = mutableListOf(24, 3, 5, 19, 200, 51) // original array
     
-    val sorted_array = merge_sort(array)
+    val sorted_array = merge_sort(array) // array after sorting
     print("sorted array = $sorted_array")
 
 }
 
 fun merge_sort(array: MutableList<Int>): MutableList<Int> {
     val len = array.size
-    if(len<=1) return array
+    if(len<=1) return array 
 
     val mid = len/2
     val left = merge_sort(array.subList(0,mid))
     val right = merge_sort(array.subList(mid, len))
 
-    return merge(left, right)
+    return merge(left, right) // merge left half and right half
 
 }
 
@@ -25,7 +25,7 @@ fun merge(left: MutableList<Int>, right: MutableList<Int>): MutableList<Int>{
     var lfidx=0
     var riidx=0
 
-    while(lfidx<left.size && riidx<right.size){
+    while(lfidx<left.size && riidx<right.size){ // compare one by one of left and right
         val lh=left[lfidx]
         val rh=right[riidx]
         if(lh<rh){
@@ -36,6 +36,7 @@ fun merge(left: MutableList<Int>, right: MutableList<Int>): MutableList<Int>{
             riidx++}
     }
 
+    // after finishing one side
     while(lfidx<left.size){
         result.add(left[lfidx])
         lfidx++
