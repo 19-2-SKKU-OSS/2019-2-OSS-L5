500b7ad67d3e0744f1addc9a9005f43af861a025
Sangwon skkusosc <mikeyoun2000@gmail.com>
2019-12-13 17:23:48 +0900

    write comments in English

--- Kotlin/CountingSort/CSmine.kt
+++ Kotlin/CountingSort/CSmine.kt
@@ -1,15 +1,15 @@
 package sort
 
 fun main(args: Array<String>){
-    val array: Array<Int> = arrayOf(24, 3, 5, 19, 200, 51)
-    val cntArray = IntArray(if(array.max()==null) 0 else array.max()!! +1)
+    val array: Array<Int> = arrayOf(24, 3, 5, 19, 200, 51) //array to sort
+    val cntArray = IntArray(if(array.max()==null) 0 else array.max()!! +1) //array to count
 
     for(num in array){
-        cntArray[num]++
+        cntArray[num]++ //count the number of value
     }
 
     var current=0
-    for(idx in 0 until cntArray.size){
+    for(idx in 0 until cntArray.size){ //change original array to sorted array
         val value = idx
         val Occurence = cntArray[idx]
         if(Occurence>0)
@@ -17,5 +17,5 @@ fun main(args: Array<String>){
                 array[current++]=value}
     }
     
-    print("sorted array = ${array.asList()}")
+    print("sorted array = ${array.asList()}") //print out
 }
