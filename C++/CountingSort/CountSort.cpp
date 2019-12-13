b73f75d0e7d75053e2a139991fdad2cc9fe21a2f
mikeysw <50179740+mikeysw@users.noreply.github.com>
2019-12-13 23:56:59 +0900

    Update CountSort.cpp

--- C++/CountingSort/CountSort.cpp
+++ C++/CountingSort/CountSort.cpp
@@ -7,16 +7,16 @@ int main(){
     int len, A[MAX_SIZE+1], B[MAX_SIZE+1], count[MAX_NUM+1], countSum[MAX_NUM+1];
     int i;
 
-    scanf("%d", &len); // length should be under MAX_SIZE
+    scanf("%d", &len); // length should be under MAX_SIZE 배열 길이는 MAX_SIZE 미만
 
     for(i=0; i<= len; i++){
-        count[i]=0; // initialize count array to 0
+        count[i]=0; // initialize count array to 0 갯수 세는 배열 0으로 초기화
     }
 
     for(i=1; i<=N; i++){
-        scanf("%d", &A[i]); // number input to array A should be under MAX_NUM
-
-        count[A[i]]++; // count how many times the number appear
+        scanf("%d", &A[i]); // number input to array A should be under MAX_NUM 배열 길이는 MAX_SIZE 미만
+ 
+        count[A[i]]++; // count how many times the number appear 해당 값이 얼마나 많이 
     }
 
     // make sum
