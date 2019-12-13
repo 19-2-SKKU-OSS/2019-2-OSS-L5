0b929911335e3fd2008cd70b962f947000569459
mikeysw <50179740+mikeysw@users.noreply.github.com>
2019-12-13 23:53:46 +0900

    Update CSmine.kt

--- Kotlin/CountingSort/CSmine.kt
+++ Kotlin/CountingSort/CSmine.kt
@@ -1,15 +1,15 @@
 package sort
 
 fun main(args: Array<String>){
-    val array: Array<Int> = arrayOf(24, 3, 5, 19, 200, 51) //array to sort
-    val cntArray = IntArray(if(array.max()==null) 0 else array.max()!! +1) //array to count
+    val array: Array<Int> = arrayOf(24, 3, 5, 19, 200, 51) //array to sort 정렬할 배열
+    val cntArray = IntArray(if(array.max()==null) 0 else array.max()!! +1) //array to count 갯수 셀 배열
 
     for(num in array){
-        cntArray[num]++ //count the number of value
+        cntArray[num]++ //count the number of value 해당 값의 갯수 세기
     }
 
     var current=0
-    for(idx in 0 until cntArray.size){ //change original array to sorted array
+    for(idx in 0 until cntArray.size){ //change original array to sorted array 원래 배열을 정렬한 배열로
         val value = idx
         val Occurence = cntArray[idx]
         if(Occurence>0)
@@ -17,5 +17,5 @@ fun main(args: Array<String>){
                 array[current++]=value}
     }
     
-    print("sorted array = ${array.asList()}") //print out
+    print("sorted array = ${array.asList()}") //print out 배열 출력
 }
