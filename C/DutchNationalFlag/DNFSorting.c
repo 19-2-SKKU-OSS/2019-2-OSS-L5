845ab017f950cb47a8b56dcafa5146d76b2a4286
mikeysw <50179740+mikeysw@users.noreply.github.com>
2019-12-13 23:54:50 +0900

    Update DNFSorting.c

--- C/DutchNationalFlag/DNFSorting.c
+++ C/DutchNationalFlag/DNFSorting.c
@@ -1,3 +1,9 @@
+//For this problem, your goal is to sort an array of 0, 1 and 2’s but you must do this in
+//place, in linear time and without any extra space (such as creating an extra array). This
+//is called the Dutch national flag sorting problem. For example, if the input array is
+//[2, 0, 0, 1, 2, 1] then your program should output [0, 0, 1, 1, 2, 2] and the algorithm should
+//run in O(n) time.
+
 #include <stdio.h>
 #include <stdlib.h>
 
